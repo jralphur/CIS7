b3dee1185f21130abe990e92b531cdd5e6443ac7
Joshua Reno <jralphur@gmail.com>
2017-09-03 17:59:15 -0700

    assignment 3

---
+++ 35.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int
+main(void)
+{
+    double x;
+    for (int i = 1; i <= 5; ++i) {
+        std::cin >> x;
+        if (((x < 5.0) && (2 * x < 10.7)) || sqrt( 5 * x ) < 5.1)
+            std::cout << x;
+    }
+}
