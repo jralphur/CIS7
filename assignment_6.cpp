70ff6eeee832fefdd63eacc97727b9ed5ad0e6fe
Joshua Reno <jralphur@gmail.com>
2017-09-13 12:14:32 -0700

    assignment 5 and 6

---
+++ assignment_6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int
+main(void)
+{
+    int d;
+    std::string buf;
+    do {
+        std::cout << "enter an integer: " << std::endl;
+        if (std::cin >> d)
+            std::cout << d << ' ' << (d % 2 ? "is odd" : "is even") << std::endl;
+        else  {
+            std::cout << "non-integer detected" << std::endl;
+            std::cin.clear();
+        }
+        std::cout << "try again? q for no, anything else for yes:" << std::endl;
+        std::cin.ignore();
+        std::getline(std::cin, buf);
+    } while (!buf.empty() && buf[0] != 'q');
+    return 0;
+
+}
