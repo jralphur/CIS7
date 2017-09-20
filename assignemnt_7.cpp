b8b4c3680e53054156b3bbbb104050e7587f0317
Joshua Reno <jralphur@gmail.com>
2017-09-20 14:52:36 -0700

    fixes

--- assignemnt_7.cpp
+++ assignemnt_7.cpp
@@ -7,22 +7,7 @@ main(void)
     int x;
     std::vector<int> squares;
 
-    for (x = 0; x * x < 170; ++x) {
-        squares.push_back(x * x);
-    }
-
-
-    auto it = squares.begin();
-    auto rit = squares.rbegin();
-
-
-    while (it != squares.end() && rit != squares.rend()) {
-        std::cout << *it + *rit << std::endl;
-        ++it; ++rit;
-    }
-
-
+    for (x = 0; x * x <= 169; ++x)
+        std::cout << x * x << std::endl;
     return 0;
-
-    
 }
