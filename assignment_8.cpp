73bfcd5e52d0b5e9f524b01f0573e8f2446e7c19
Joshua Reno <jralphur@gmail.com>
2017-09-20 15:43:29 -0700

    less worse but not better for loop for assignment 8

--- assignment_8.cpp
+++ assignment_8.cpp
@@ -4,18 +4,10 @@
 bool 
 isprime(size_t i)
 {
-    if (i <= 1)
-        return false;
-    else if (i <= 3)
-        return true;
-    else if (i % 2 == 0 || i % 3 == 0)
-        return false;
-    size_t n = 5;
-    while (i * i <= n) {
-        if (n % i == 0 || n % (i + 2) == 0)
+    if (i <= 1) return false;
+    for (size_t k = 2; k < i; ++k)
+        if (i % k == 0)
             return false;
-        i += 6;
-    }
     return true;
 }
 
@@ -30,7 +22,10 @@ main(void)
     std::cin >> upper;
 
     for (size_t i = 0; i <= upper; ++i)
-        if (isprime(i)) actual++;
+        if (isprime(i)) {
+            std::cout << i << std::endl;
+            ++actual;
+        }
 
     std::cout << "How many prime numbers exist? Guess: ";
     std::cin >> guess;
